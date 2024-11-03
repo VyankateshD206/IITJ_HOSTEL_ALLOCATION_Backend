0e62fc9e77deb3c45ecbf71ee793caa6d8e29c11
Vyankatesh Deshpande <b23cs1079@iitj.ac.in>
2024-11-04 00:15:18 +0530

    added base url

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ app.use(cors({
 
 
 mongoose.connect(process.env.MONGO_URL);
+app.get('/', (req,res) => {
+  res.send("Hostel Room Allocation");
+});
 app.get('/test', (req,res) => {
   res.json('test ok');
 });
