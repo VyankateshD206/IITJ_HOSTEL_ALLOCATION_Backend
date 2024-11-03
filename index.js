f9896ba311c5e7f7730aee5f7b5bb6185035e5da
Vyankatesh Deshpande <b23cs1079@iitj.ac.in>
2024-11-04 00:51:56 +0530

    changes

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
 app.use(express.json());
 app.use(cookieParser());
 // // app.use('/uploads', express.static(__dirname+'/uploads'));
-app.use(cors());
+app.use(cors({
+  origin: '*' // Allows all origins but blocks credentials
+}));
 
 
 mongoose.connect(process.env.MONGO_URL);
